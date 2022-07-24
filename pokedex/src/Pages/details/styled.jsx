096fced1b7328f0091e68cc4f5355f7f237668ec
andrebalsemao <andrebalsemao@gmail.com>
2022-07-24 20:58:58 -0300

    Projeto Finalizado

--- pokedex/src/Pages/details/styled.jsx
+++ pokedex/src/Pages/details/styled.jsx
@@ -3,27 +3,30 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  align-items: center;
+  flex-direction: column;
   background: #9c9b9b;
-  width: 98vw;
+  width: 97vw;
   height: 80vh;
   padding-left: 8px;
 
   h1 {
     position: absolute;
     color: white;
-    font-size: 56px;
+    font-size: 44px;
   }
 
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
-    flex-direction: column;
-    height: 70vh;
     grid-template-columns: 1fr;
+    width: 97vw;
+    height:200vh;
   }
 
-  @media screen and (min-device-width: 481px) and (max-device-width: 800px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
+  h1 {
+    position: absolute;
+    color: white;
+    font-size: 44px;
+    margin-top: 5px;
   }
 `;
 
@@ -70,127 +73,104 @@ export const Properties = styled.div`
     }
   }};
   border-radius: 38px;
-  width: 95%;
-  height: 70%;
-  margin-top: 15vh;
+  width: 95vw;
+  height: 65vh;
+  margin-top: 10vh;
   margin-left: 0;
 
-  @media (max-width: 400px) {
-    height: 71.6vh;
-  }
-  @media (max-width: 340px) {
-    height: 74vh;
-  }
-
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
-  }
-
-  @media screen and (min-device-width: 481px) and (max-device-width: 800px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
+    grid-template-columns: 1fr;
+    flex-direction: column-reverse;
+    width: 90vw;
+    height: 178vh;
+    justify-content: flex-end;
   }
 `;
 
 export const MovesBack = styled.div`
   display: flex;
-  width: 20%;
-  height: 90%;
-  margin-left: 3%;
-  margin-top: 5%;
+  width: 200px;
+  height: 600px;
+  margin-left: 10px;
+  margin-top: 104px;
   flex-direction: column;
 `;
 export const PokeProperties = styled.div`
   display: flex;
-  color: white;
-  width: 70%;
-  height: 28%;
+  color: #ffffff;
+  width: 10px;
   flex-direction: column;
   z-index: 2;
 
   h2 {
+    display: flex;
+    margin: 0 10px 0 10px;
     font-size: 48px;
-    margin-top: -19%;
-    z-index: 2;
   }
 
   h3 {
+    display: flex;
     font-size: 24px;
+    margin: 0 50px 0 10px;
     z-index: 2;
   }
 
   p {
-    margin-bottom: 1%;
     width: 60px;
     z-index: 2;
   }
 
   div {
     display: flex;
-    row-gap: 20%;
-    width: 100%;
+    row-gap: 30px;
+    width: 200px;
     justify-content: space-around;
-  }
-  @media (max-width: 400px) {
-    height: 71.6vh;
-  }
-  @media (max-width: 340px) {
-    height: 74vh;
   }
 
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
+    width: 280px;
+    margin-left: 0px;
     flex-direction: column;
-    display: block;
-    height: 70vh;
-  }
+    margin-top: 2px;
+    margin-left:2px;
+    
 
-  @media screen and (min-device-width: 481px) and (max-device-width: 800px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
+    h2 {
+    display: flex;
+    margin-top: 26px;
+    font-size: 40px;
+    }
   }
 `;
 export const PokeMovies = styled.div`
   display: flex;
   flex-direction: column;
   background-color: white;
-  width: 100%;
-  height: 61%;
+  width: 320px;
+  height: 370px;
   border-radius: 8px;
   padding-left: 2px;
-  margin-left: 5%;
-  margin-top: 20%;
+  margin-left: 15px;
+  margin-top: 36px;
   z-index: 0;
 
   p p {
-    width: fit-content;
-    background-color:  #9c9b9b6e;
-    border: 2px solid black;
-    border-radius: 8px;
-    padding: 5px;
+    margin-left: 2px;
+    width: 160px;
+    background-color: #9c9b9b6e;
+    border: 1px solid black;
+    border-radius: 16px;
+    padding: 7px;
   }
-
-  @media (max-width: 400px) {
-    height: 71.6vh;
-  }
-  @media (max-width: 340px) {
-    height: 74vh;
-  }
-
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
+    width: 280px;
+    margin-top: 12px;
     flex-direction: column;
-    display: block;
-    height: 70vh;
+    ul {
+    margin-top: 1px;
   }
+}
 
-  @media screen and (min-device-width: 481px) and (max-device-width: 800px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
-  }
 `;
 
 export const PokeStats = styled.div`
@@ -199,45 +179,39 @@ export const PokeStats = styled.div`
   flex-direction: column;
   background-color: white;
   border-radius: 8px;
-  width: 24%;
-  height: 90%;
-  margin-left: 3%;
-  margin-top: 3%;
+  width: 320px;
+  height: 600px;
+  margin-left: 20px;
+  margin-top: 34px;
   z-index: 2;
 
   li {
     display: flex;
     justify-content: space-evenly;
-    width: 60%;
+    width: 200px;
     font-size: 16px;
     z-index: 1;
   }
 
-  @media (max-width: 400px) {
-    height: 71.6vh;
-  }
-  @media (max-width: 340px) {
-    height: 74vh;
+    @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
+    width: 280px;
+    height: 450px;
+    margin-left: 26px;
+    margin-top: 8px;
+    flex-direction: column;
+    
+    li {
+    margin-top: 1px;
   }
 
-  @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
-  }
-
-  @media screen and (min-device-width: 481px) and (max-device-width: 800px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
   }
 `;
 
 export const PokeImage = styled.div`
   display: flex;
   justify-content: space-around;
-  width: 24%;
-  height: 100%;
+  width: 280px;
+  height: 665px;
   align-items: center;
   flex-direction: column;
 
@@ -246,97 +220,85 @@ export const PokeImage = styled.div`
     display: flex;
     background-color: white;
     border-radius: 8px;
-    width: 90%;
-    height: 40%;
+    width: 260px;
+    height: 265px;
   }
 
   img {
     display: flex;
     width: 128px;
     height: 100px;
-    margin-top: 88px;
+    margin-top: 80px;
     margin-left: 60px;
   }
 
-  @media (max-width: 400px) {
-    height: 71.6vh;
-  }
-  @media (max-width: 340px) {
-    height: 74vh;
-  }
-
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
-  }
+    display: flex;
+    grid-template-columns: 2fr;
+    width: 88vw;
+    flex-direction: row;
+    align-items: center;
 
-  @media screen and (min-device-width: 481px) and (max-device-width: 800px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
+    img {
+      width: 80px;
+      height: 80px;
+      margin-top: 10px;
+      margin-left: 14px;
+    }
+
+    div {
+      width: 100px;
+      height: 100px;
+      
+    }
   }
 `;
 
 export const ImageBig = styled.div`
   display: flex;
   position: absolute;
-  margin-left: 71%;
+  margin-left: 950px;
+  margin-top: 40px;
   width: 250px;
   height: 150px;
   flex-direction: column-reverse;
   justify-content: flex-start;
 
-  @media (max-width: 400px) {
-    height: 71.6vh;
-  }
-  @media (max-width: 340px) {
-    height: 74vh;
-  }
-
   @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
-  }
-
-  @media screen and (min-device-width: 481px) and (max-device-width: 800px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
+    display: flex;
+    grid-template-columns: 1fr;
+    width: 150px;
+    height: 150px;
+    align-items: stretch;
+    margin-left: 100px;
+    margin-top: 0px;
+    justify-content: flex-end;
+    
   }
 `;
 
 export const ImageBack = styled.div`
   display: flex;
-
-  @media (max-width: 400px) {
-    height: 71.6vh;
-  }
-  @media (max-width: 340px) {
-    height: 74vh;
-  }
-
-  @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
-  }
-
-  @media screen and (min-device-width: 481px) and (max-device-width: 800px) {
-    flex-direction: column;
-    display: block;
-    height: 70vh;
-  }
 `;
 export const Pokebola = styled.div`
   position: absolute;
-  margin-left: 42%;
-  width: 51%;
-  margin-top: 2%;
+  margin-left: 500px;
+  width: 750px;
+  margin-top: 38px;
   img {
-    width: 96%;
+    width: 718px;
     z-index: -1;
   }
 
+  @media screen and (min-device-width: 320px) and (max-device-width: 480px) {
+    position: absolute;
+  margin-left: 50px;
+  width: 275px;
+  margin-top: 38px;
+  img {
+    width: 220px;
+    z-index: -1;
+  }
+
+  }
 `;
